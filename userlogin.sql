5f96364a30e066c85cb983d7a30116ff9870fe16
Furkan Arda <45922246+vevampire-tr@users.noreply.github.com>
2021-03-05 13:20:33 +0300

    Add files via upload

---
+++ userlogin.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 05 Mar 2021, 11:20:21
+-- Sunucu sürümü: 10.4.17-MariaDB
+-- PHP Sürümü: 7.4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `userlogin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `tbl_auth`
+--
+
+CREATE TABLE `tbl_auth` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
+  `created_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `tbl_auth`
+--
+ALTER TABLE `tbl_auth`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `tbl_auth`
+--
+ALTER TABLE `tbl_auth`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
